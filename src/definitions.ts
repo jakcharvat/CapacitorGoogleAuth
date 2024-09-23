b8f2181d840a5f7b641da8a131b5aeaa461e129c
Jakub Charvát <jakcharvat@gmail.com>
2024-09-23 23:48:32 +0200

    Define plugin options interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,10 @@
+/// <reference types="@capacitor/cli" />
+declare module '@capacitor/cli' {
+  export interface PluginsConfig {
+    GoogleAuth: GoogleAuthPluginOptions;
+  }
+}
+
 export interface User {
   /**
    * The unique identifier for the user.
