473d50d6b518d27d16f00319a7a2f1e949fbe936
Pavel Holman <pavelholman@email.cz>
2024-09-24 23:22:08 +0200

    Add GoogleAuth plugin

--- android/src/main/java/dev/jakcharvat/capacitorgoogleauth/GoogleAuthPlugin.kt
+++ android/src/main/java/dev/jakcharvat/capacitorgoogleauth/GoogleAuthPlugin.kt
@@ -5,17 +5,24 @@ import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
 import com.getcapacitor.PluginMethod
 import com.getcapacitor.annotation.CapacitorPlugin
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @CapacitorPlugin(name = "GoogleAuth")
 class GoogleAuthPlugin : Plugin() {
-    private val implementation = GoogleAuth()
+    private val implementation = GoogleAuth(this.activity.applicationContext)
 
     @PluginMethod
-    fun echo(call: PluginCall) {
-        val value: String? = call.getString("value")
+    suspend fun signIn(call: PluginCall) {
+        when (val user = implementation.signIn()) {
+            is GoogleAuth.User -> call.resolve(JSObject(Json.encodeToString(user)))
+            else -> call.reject("Sign-in failed")
+        }
+    }
 
-        val ret = JSObject()
-        ret.put("value", value?.let { implementation.echo(it) })
-        call.resolve(ret)
+    @PluginMethod
+    suspend fun signOut(call: PluginCall) {
+        implementation.signOut()
+        call.resolve()
     }
 }
