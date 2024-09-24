06674e75432e1e8a9061f9cdb42a1e8090c038d9
Jakub Charvát <jakcharvat@gmail.com>
2024-09-24 18:47:56 +0200

    Add stub implementations for web plugin

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,15 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { GoogleAuthPlugin } from './definitions';
+import type { GoogleAuthPlugin, InitOptions, User } from './definitions';
 
 export class GoogleAuthWeb extends WebPlugin implements GoogleAuthPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  initialize(_options?: InitOptions): Promise<void> {
+    throw this.unimplemented('Initialize is not yet available on the web');
+  }
+  signIn(): Promise<User> {
+    throw this.unimplemented('Sign in is not yet available on the web');
+  }
+  signOut(): Promise<void> {
+    throw this.unimplemented('Sign out is not yet available on the web');
   }
 }
