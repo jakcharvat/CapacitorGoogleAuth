b08a73bda61ce849943ef224527fe649ce48f8af
Jakub Charvát <jakcharvat@gmail.com>
2024-09-29 14:22:36 +0200

    Downgrade to GoogleSignIn-iOS 7.1.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
-        .package(url: "https://github.com/google/GoogleSignIn-iOS", .upToNextMinor(from: "8.0.0")),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS", .upToNextMinor(from: "7.1.0")),
     ],
     targets: [
         .target(
